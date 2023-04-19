abda361ed458d97a633af1b8c4c98c6435bbd91a
Bollsjen <55839056+Bollsjen@users.noreply.github.com>
2023-04-19 20:36:58 +0200

    SU-2-MB [Feature] Common table

---
+++ frontend/webapp/src/components/common/CommonTable.vue
@@ -0,0 +1,56 @@
+<template>
+  <b-table :fields="fields" :items="items">
+    <template #head(actions)="data">
+      {{data.label}}
+    </template>
+
+    <template #cell(actions)="data">
+      <div :class="getPlacement(data.field.template.cell[0].placement)">
+        <b-button v-for="(cell, index) in data.field.template.cell"
+                  :key="index"
+                  :variant="cell.variant"
+                  :size="cell.size"
+                  style="margin-left: 5px"
+                  :class="getPlacement(cell.placement)"
+                  @click="cell.action(data.item)">
+          <i class="fa-solid" :class="cell.icon"></i>
+        </b-button>
+      </div>
+    </template>
+
+  </b-table>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  props: {
+    fields: Array,
+    items: Array,
+  },
+  data(){
+    return {
+    }
+  },
+  methods: {
+    getPlacement(placement){
+      if(placement === 'left'){
+        console.log('start')
+        return 'text-start'
+      }else if (placement === 'right'){
+        console.log('end')
+        return 'text-end'
+      }
+      console.log('center | ' + placement)
+      return 'text-center'
+    }
+  },
+  computed: {
+
+  },
+  mounted() {
+  }
+}
+</script>
