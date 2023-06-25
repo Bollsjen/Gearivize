d0f11f8067b90bd88bcb77030d529008b8962972
Bollsjen <55839056+Bollsjen@users.noreply.github.com>
2023-06-25 23:54:30 +0200

    Common table selectable fields added

--- frontend/webapp/src/views/TemplatesPage.vue
+++ frontend/webapp/src/views/TemplatesPage.vue
@@ -1,30 +1,76 @@
 <template>
   <b-container fluid class="w-75 text-start">
     <h1>Templates</h1>
+
+    <common-table :fields="fields1" :items="instruments" selectable :row-selected="rowSelected"/>
+
+    <common-table :fields="fields2" :items="selectedInstruments" />
   </b-container>
 </template>
 
 <script>
+import CommonTable from "@/components/common/CommonTable.vue";
+import {instrumentsService} from "@/services/instrumentsService";
+
 export default {
   components: {
-
+    CommonTable
   },
   props: {
 
   },
   data(){
     return {
-
+      instruments: [],
+      selectedInstruments: []
     }
   },
   methods: {
+    load(){
+      instrumentsService.getAll()
+          .then(result => this.instruments = result.data)
+          .catch(error => console.error(error))
+    },
 
+    rowSelected(data){
+      this.selectedInstruments = data
+    }
   },
   computed: {
+    fields1(){
+      return [
+        {
+          key: 'selected',
+          label: ''
+        },
+        {
+          key: 'aNumber'
+        },
+        {
+          key: 'instrumentName',
+        },
+        {
+          key: 'serialNumber'
+        }
+      ]
+    },
 
+    fields2(){
+      return [
+        {
+          key: 'aNumber'
+        },
+        {
+          key: 'instrumentName',
+        },
+        {
+          key: 'serialNumber'
+        }
+      ]
+    }
   },
   mounted() {
-
+    this.load()
   }
 }
 </script>
